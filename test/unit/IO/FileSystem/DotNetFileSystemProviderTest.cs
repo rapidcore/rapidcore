e0763a8527f02015ec2ef3571bae090d62b6a665
Rui Martins <rui@skarp.dk>
2017-05-18 11:12:16 +0200

    Test Move method

--- test/unit/IO/FileSystem/DotNetFileSystemProviderTest.cs
+++ test/unit/IO/FileSystem/DotNetFileSystemProviderTest.cs
@@ -11,10 +11,16 @@ namespace RapidCore.UnitTests.IO.FileSystem
     public class DotNetFileSystemProviderTest
     {
         private DotNetFileSystemProvider _fileSystem;
+        private string _assemblyPath;
+        private string _filesPath;
+        private string _originFilesPath;
 
         public DotNetFileSystemProviderTest()
         {
             _fileSystem = new DotNetFileSystemProvider();
+            _assemblyPath = Path.GetDirectoryName(typeof(DotNetFileSystemProviderTest).GetTypeInfo().Assembly.Location);
+            _filesPath = Path.Combine(_assemblyPath, $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}Files");
+            _originFilesPath = Path.Combine(_assemblyPath, $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}OriginFolder");
         }
 
         [Fact]
@@ -28,29 +34,43 @@ namespace RapidCore.UnitTests.IO.FileSystem
         [Fact]
         public void Can_ListDirectory()
         {
-            var assemblyPath = Path.GetDirectoryName(typeof(DotNetFileSystemProviderTest).GetTypeInfo().Assembly.Location);
-
-            var localFilesPath = Path.Combine(assemblyPath, $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}Files");
-            var files = _fileSystem.List(localFilesPath);
+            var files = _fileSystem.List(_filesPath);
 
             Assert.NotNull(files);
             Assert.NotEmpty(files);
-            Assert.Contains(Path.Combine(localFilesPath, $"CsvFile1.csv"), files);
-            Assert.Contains(Path.Combine(localFilesPath, $"TextFile1.txt"), files);   
+            Assert.Contains(Path.Combine(_filesPath, $"CsvFile1.csv"), files);
+            Assert.Contains(Path.Combine(_filesPath, $"TextFile1.txt"), files);   
         }
 
         [Fact]
         public void Can_ListDirectoryWithSearchPattern()
         {
-            var assemblyPath = Path.GetDirectoryName(typeof(DotNetFileSystemProviderTest).GetTypeInfo().Assembly.Location);
-
-            var localFilesPath = Path.Combine(assemblyPath, $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}Files");
-            var files = _fileSystem.List(localFilesPath, "*.csv");
+            var files = _fileSystem.List(_filesPath, "*.csv");
 
             Assert.NotNull(files);
             Assert.NotEmpty(files);
-            Assert.Contains(Path.Combine(localFilesPath, $"CsvFile1.csv"), files);
-            Assert.DoesNotContain(Path.Combine(localFilesPath, $"TextFile1.txt"), files);
+            Assert.Contains(Path.Combine(_filesPath, $"CsvFile1.csv"), files);
+            Assert.DoesNotContain(Path.Combine(_filesPath, $"TextFile1.txt"), files);
+        }
+
+        [Fact]
+        public void Can_MoveFileToFolder()
+        {
+            var srcFile = Path.Combine(_originFilesPath, "FileToMove.txt");
+            var dstFile = Path.Combine(_filesPath, "FileToMove.txt");
+            _fileSystem.Move(srcFile, dstFile);
+
+            var srcFolder = Directory.EnumerateFiles(_originFilesPath);
+            var dstFolder = Directory.EnumerateFiles(_filesPath);
+
+            Assert.DoesNotContain(srcFile, srcFolder);
+            Assert.Contains(dstFile, dstFolder);
+
+            _fileSystem.Move(dstFile, srcFile);
+
+            Assert.Contains(srcFile, srcFolder);
+            Assert.DoesNotContain(dstFile, dstFolder);
+
         }
     }
 }
