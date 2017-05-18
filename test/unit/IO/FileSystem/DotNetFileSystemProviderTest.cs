062103b663d86b7f0379a87d4227447f7c84de7b
Rui Martins <rui@skarp.dk>
2017-05-18 11:46:57 +0200

    Test rest of DotNetFileSystemProvider methods

--- test/unit/IO/FileSystem/DotNetFileSystemProviderTest.cs
+++ test/unit/IO/FileSystem/DotNetFileSystemProviderTest.cs
@@ -12,14 +12,28 @@ namespace RapidCore.UnitTests.IO.FileSystem
     {
         private DotNetFileSystemProvider _fileSystem;
         private string _assemblyPath;
+        // the full path: e.g. C:/.../.../bin/debug/IO/FileSystem/Files
         private string _filesPath;
+        // the local IO/FileSystem/OriginFolder
         private string _originFilesPath;
+        // the local IO/FileSystem/Files
         private string _localFilesFolder;
+        // textFile1.txt
+        private string _textFileName;
+        // CsvFile1.csv
+        private string _csvFileName;
+        // FileToMove.txt
+        private string _fileToMoveName;
 
         public DotNetFileSystemProviderTest()
         {
             _fileSystem = new DotNetFileSystemProvider();
             _assemblyPath = Path.GetDirectoryName(typeof(DotNetFileSystemProviderTest).GetTypeInfo().Assembly.Location);
+
+            _textFileName = "TextFile1.txt";
+            _csvFileName = "CsvFile1.csv";
+            _fileToMoveName = "FileToMove.txt";
+
             _localFilesFolder = $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}Files";
             _filesPath = Path.Combine(_assemblyPath, _localFilesFolder);
             _originFilesPath = Path.Combine(_assemblyPath, $"IO{Path.DirectorySeparatorChar}FileSystem{Path.DirectorySeparatorChar}OriginFolder");
@@ -40,8 +54,8 @@ namespace RapidCore.UnitTests.IO.FileSystem
 
             Assert.NotNull(files);
             Assert.NotEmpty(files);
-            Assert.Contains(Path.Combine(_filesPath, $"CsvFile1.csv"), files);
-            Assert.Contains(Path.Combine(_filesPath, $"TextFile1.txt"), files);   
+            Assert.Contains(Path.Combine(_filesPath, _csvFileName), files);
+            Assert.Contains(Path.Combine(_filesPath, _textFileName), files);   
         }
 
         [Fact]
@@ -51,15 +65,15 @@ namespace RapidCore.UnitTests.IO.FileSystem
 
             Assert.NotNull(files);
             Assert.NotEmpty(files);
-            Assert.Contains(Path.Combine(_filesPath, $"CsvFile1.csv"), files);
-            Assert.DoesNotContain(Path.Combine(_filesPath, $"TextFile1.txt"), files);
+            Assert.Contains(Path.Combine(_filesPath, _csvFileName), files);
+            Assert.DoesNotContain(Path.Combine(_filesPath, _textFileName), files);
         }
 
         [Fact]
         public void Can_MoveFileToFolder()
         {
-            var srcFile = Path.Combine(_originFilesPath, "FileToMove.txt");
-            var dstFile = Path.Combine(_filesPath, "FileToMove.txt");
+            var srcFile = Path.Combine(_originFilesPath, _fileToMoveName);
+            var dstFile = Path.Combine(_filesPath, _fileToMoveName);
             _fileSystem.Move(srcFile, dstFile);
 
             var srcFolder = Directory.EnumerateFiles(_originFilesPath);
@@ -78,6 +92,51 @@ namespace RapidCore.UnitTests.IO.FileSystem
         public void Can_CombinePaths()
         {
             Assert.Equal(_fileSystem.CombinePath(_assemblyPath, _localFilesFolder), Path.Combine(_assemblyPath, _localFilesFolder));
+        }
+
+        [Fact]
+        public void Can_OpenReadAFile()
+        {
+            var fileContent = "my content";
+
+            var stream = _fileSystem.OpenReadFile(Path.Combine(_filesPath, _textFileName));
+            string content = new StreamReader(stream, Encoding.UTF8).ReadToEnd();
+            Assert.Equal(content, fileContent);
+
+        }
+
+        [Fact]
+        public void Can_LoadContentOfAFile()
+        {
+            var fileContent = "my content";
+            var returnedContent = _fileSystem.LoadContent(Path.Combine(_filesPath, _textFileName));
+            Assert.Equal(returnedContent, fileContent);
+        }
+
+        [Fact]
+        public void Can_GetFileName()
+        {
+            var path = Path.Combine(_filesPath, _textFileName);
+            var filename = _fileSystem.GetFileName(path);
+
+            Assert.Equal(filename, _textFileName);
+        }
+
+        [Fact]
+        public void Can_DetermineIfDirectoryExists()
+        {
+            Assert.True(_fileSystem.DirectoryExists(_filesPath));
+            Assert.False(_fileSystem.DirectoryExists(Path.Combine(_assemblyPath, "FakePath")));
+        }
+
+        [Fact]
+        public void Can_CreateDirectory()
+        {
+            var path = Path.Combine(_filesPath, "tmpDir");
+            _fileSystem.CreateDirectory(path);
+
+            Assert.True(Directory.Exists(path));
+            Directory.Delete(path);
         }
     }
 }
