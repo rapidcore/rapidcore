025a009fb9acbbf2107f687c17c677d4a4f86ce4
Patrick Boisen <patrick@skarp.dk>
2017-05-19 14:58:51 +0200

    Add Type.GetMethodRecursively(..) ext.method

---
+++ src/Reflection/TypeGetMethodRecursivelyExtensions.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace RapidCore.Reflection
+{
+    /// <summary>
+    /// Extension methods for getting a method from a <see cref="System.Type" />
+    /// </summary>
+    public static class TypeGetMethodRecursivelyExtensions
+    {
+        /// <summary>
+        /// Get a method recursively
+        /// </summary>
+        /// <param name="type">The type to work on</param>
+        /// <param name="methodName">The name of the method to get</param>
+        /// <param name="argTypes">The type of the arguments you would use</param>
+        /// <returns>A <see cref="System.Reflection.MethodInfo" /></returns>
+        /// <exception cref="System.MissingMethodException">Thrown if the method could not be found</exception>
+        public static MethodInfo GetMethodRecursively(this Type type, string methodName, params Type[] argTypes)
+        {
+            var typeInfo = type.GetTypeInfo();
+            var method = typeInfo.GetMethod(methodName, argTypes);
+
+            if (method == null && typeInfo.BaseType != null)
+            {
+                method = typeInfo.BaseType.GetMethodRecursively(methodName, argTypes);
+            }
+
+            if (method == null)
+            {
+                throw new MissingMethodException($"Could not find method {GenerateMethodParamsSignature(methodName, argTypes)}");
+            }
+
+            return method;
+        }
+
+        private static string GenerateMethodParamsSignature(string methodName, Type[] argTypes)
+        {
+            var typeSignature = string.Join(", ", argTypes.Select(t => t.Name));
+            return $"{methodName}({typeSignature})";
+        }
+    }
+}
---
+++ test/unit/Reflection/TypeGetMethodRecursivelyExtensionsTests.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Reflection;
+using RapidCore.Reflection;
+using Xunit;
+
+namespace RapidCore.UnitTests.Reflection
+{
+    public class TypeGetMethodRecursivelyExtensionsTests
+    {
+        [Fact]
+        public void GetMethodRecursively_FindsMethodsWithParams_InFirstLayer()
+        {
+            var actual = typeof(GuineaPig).GetMethodRecursively("OneParam", typeof(string));
+            
+            Assert.NotNull(actual);
+            Assert.Equal("OneParam", actual.Name);
+        }
+
+        [Fact]
+        public void GetMethodRecursively_FindsMethodsWithoutParams_InFirstLayer()
+        {
+            var actual = typeof(GuineaPig).GetMethodRecursively("ZeroParams");
+            
+            Assert.NotNull(actual);
+            Assert.Equal("ZeroParams", actual.Name);
+        }
+
+        [Fact]
+        public void GetMethodRecursively_FindsOverloadedMethodsWithoutParams()
+        {
+            var actual = typeof(GuineaPig).GetMethodRecursively("HasOverload");
+            
+            Assert.NotNull(actual);
+            Assert.Equal("HasOverload", actual.Name);
+            Assert.Empty(actual.GetParameters());
+        }
+
+        [Fact]
+        public void GetMethodRecursively_FindsOverloadedMethodsWithParams()
+        {
+            var actual = typeof(GuineaPig).GetMethodRecursively("HasOverload", typeof(Attribute));
+            
+            Assert.NotNull(actual);
+            Assert.Equal("HasOverload", actual.Name);
+            Assert.Equal(1, actual.GetParameters().Length);
+        }
+
+        [Fact]
+        public void GetMethodRecursively_FindsMethodsWithoutParams_InNextLayer()
+        {
+            var actual = typeof(GuineaPig).GetMethodRecursively("GetType");
+            
+            Assert.NotNull(actual);
+            Assert.Equal("GetType", actual.Name);
+            Assert.Equal(typeof(Object), actual.DeclaringType);
+        }
+
+        [Fact]
+        public void GetMethodRecursively_Throws_ifMethodDoesNotExist_withoutParams()
+        {
+            try
+            {
+                typeof(GuineaPig).GetMethodRecursively("DoesNotExist");
+            }
+            catch (MissingMethodException ex)
+            {
+                Assert.Equal("Could not find method DoesNotExist()", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void GetMethodRecursively_Throws_ifMethodDoesNotExist_singleParam()
+        {
+            try
+            {
+                typeof(GuineaPig).GetMethodRecursively("DoesNotExist", typeof(long));
+            }
+            catch (MissingMethodException ex)
+            {
+                Assert.Equal("Could not find method DoesNotExist(Int64)", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void GetMethodRecursively_Throws_ifMethodDoesNotExist_multipleParams()
+        {
+            try
+            {
+                typeof(GuineaPig).GetMethodRecursively("DoesNotExist", typeof(long), typeof(int), typeof(Attribute));
+            }
+            catch (MissingMethodException ex)
+            {
+                Assert.Equal("Could not find method DoesNotExist(Int64, Int32, Attribute)", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void GetMethodRecursively_Throws_ifMethodDoesNotExist_withThoseParameters()
+        {
+            try
+            {
+                typeof(GuineaPig).GetMethodRecursively("HasOverload", typeof(long), typeof(int), typeof(Attribute));
+            }
+            catch (MissingMethodException ex)
+            {
+                Assert.Equal("Could not find method HasOverload(Int64, Int32, Attribute)", ex.Message);
+            }
+        }
+
+        #region GuineaPig
+        private class GuineaPig
+        {
+            public void OneParam(string a) { }
+
+            public void ZeroParams() { }
+
+            public void HasOverload() { }
+
+            public void HasOverload(Attribute attrib) { }
+        }
+        #endregion
+    }
+}
