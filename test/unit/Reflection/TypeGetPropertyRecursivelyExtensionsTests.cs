8b259216f5cf963101f82acef6ab9e676a367e2a
Patrick Boisen <patrick@skarp.dk>
2017-05-19 15:37:28 +0200

    Add Type.GetPropertyRecursively(...)

---
+++ test/unit/Reflection/TypeGetPropertyRecursivelyExtensionsTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Reflection;
+using RapidCore.Reflection;
+using Xunit;
+
+namespace RapidCore.UnitTests.Reflection
+{
+    public class TypeGetPropertyRecursivelyExtensionsTests
+    {
+        [Fact]
+        public void GetPropertyRecursively_canFindIt_inFirstLayer()
+        {
+            var actual = typeof(GuineaPig).GetPropertyRecursively("FirstLayer");
+
+            Assert.Equal("FirstLayer", actual.Name);
+        }
+
+        [Fact]
+        public void GetPropertyRecursively_canFindIt_inNextLayer()
+        {
+            var actual = typeof(GuineaPig).GetPropertyRecursively("SecondLayer");
+
+            Assert.Equal("SecondLayer", actual.Name);
+        }
+
+        [Fact]
+        public void GetPropertyRecursively_throwsIfNoSuchProperty()
+        {
+            try
+            {
+                typeof(GuineaPig).GetPropertyRecursively("DoesNotExist");
+            }
+            catch (MissingMemberException ex)
+            {
+                Assert.Equal("Could not find a property called DoesNotExist", ex.Message);
+            }
+        }
+
+        #region GuineaPig
+        private class GuineaPig : GuineaPigBase
+        {
+            public string FirstLayer { get; set; }
+        }
+
+        private abstract class GuineaPigBase
+        {
+            public int SecondLayer { get; set; }
+        }
+        #endregion
+    }
+}
