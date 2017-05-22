7801e7d58a4c6ecf9fc7bc1ccfe9dacbaecccd53
Patrick Boisen <patrick@skarp.dk>
2017-05-22 09:54:55 +0200

    Split simple and compound index tests

--- test/functional/MongoManager_EnsureIndexes_Tests.cs
+++ test/functional/MongoManager_EnsureIndexes_Tests.cs
@@ -28,7 +28,36 @@ namespace RapidCore.Mongo.FunctionalTests
                 typeof(SimpleIndexes).GetTypeInfo().Namespace
             );
 
-            var actual = GetDb().GetCollection<SimpleIndexes>(collectionName).Indexes.List();
+            var indexes = GetIndexes<SimpleIndexes>(collectionName);
+
+            Assert.Equal(3, indexes.Count); // the auto-generated "_id_" and our own
+            Assert.Equal(new BsonDocument().Add("String", 1), indexes["string_index"].GetElement("key").Value);
+            Assert.Equal(new BsonDocument().Add("Int", 1), indexes["Int_1"].GetElement("key").Value);
+        }
+
+        [Fact]
+        public void CanCreateCompoundIndexes()
+        {
+            var collectionName = typeof(CompoundIndexes).GetTypeInfo().GetCollectionName();
+            GetDb().DropCollection(collectionName);
+            GetDb().CreateCollection(collectionName);
+
+            manager.EnsureIndexes(
+                GetDb(),
+                typeof(CompoundIndexes).GetTypeInfo().Assembly,
+                typeof(CompoundIndexes).GetTypeInfo().Namespace
+            );
+
+            var indexes = GetIndexes<CompoundIndexes>(collectionName);
+
+            Assert.Equal(2, indexes.Count); // the auto-generated "_id_" and our own
+            Assert.Equal(new BsonDocument().Add("CompoundnessOfTheLong", 1).Add("StringOfCompoundness", 1), indexes["compound_index"].GetElement("key").Value);
+        }
+
+        #region GetIndexes
+        private IDictionary<string, BsonDocument> GetIndexes<TDocument>(string collectionName)
+        {
+            var actual = GetDb().GetCollection<TDocument>(collectionName).Indexes.List();
             actual.MoveNext();
             
             var indexes = new Dictionary<string, BsonDocument>();
@@ -38,22 +67,29 @@ namespace RapidCore.Mongo.FunctionalTests
                 indexes[index.GetElement("name").Value.AsString] = index;
             }
 
-            Assert.Equal(4, indexes.Count); // the auto-generated "_id_" and our own
-            Assert.Equal(new BsonDocument().Add("String", 1), indexes["string_index"].GetElement("key").Value);
-            Assert.Equal(new BsonDocument().Add("Int", 1), indexes["Int_1"].GetElement("key").Value);
-            Assert.Equal(new BsonDocument().Add("Int", 1).Add("String", 1), indexes["compound_index"].GetElement("key").Value);
+            return indexes;
         }
+        #endregion
 
         #region Simple indexes
         private class SimpleIndexes
         {
             [Index("string_index")]
-            [Index("compound_index", Order = 2)]
             public string String { get; set; }
 
             [Index]
-            [Index("compound_index", Order = 1)]
             public int Int { get; set; }
+        }
+        #endregion
+
+        #region Compound indexes
+        private class CompoundIndexes
+        {
+            [Index("compound_index", Order = 2)]
+            public string StringOfCompoundness { get; set; }
+
+            [Index("compound_index", Order = 1)]
+            public long CompoundnessOfTheLong { get; set; }
         }
         #endregion
     }
