31f89dcec8560026828b20619f5888b042586cbb
Patrick Boisen <patrick@skarp.dk>
2017-05-22 11:23:15 +0200

    Add tests showing support for [Index] on parent

--- test/functional/MongoManager_EnsureIndexes_Tests.cs
+++ test/functional/MongoManager_EnsureIndexes_Tests.cs
@@ -34,6 +34,25 @@ namespace RapidCore.Mongo.FunctionalTests
             Assert.Equal(new BsonDocument().Add("CompoundnessOfTheLong", 1).Add("StringOfCompoundness", 1), actual["compound_index"].GetElement("key").Value);
         }
 
+        [Fact]
+        public void CreatesIndexesFromAllLevels()
+        {
+            var actual = CreateAndGetIndexes<MultiLevelTwo>();
+
+            Assert.Equal(4, actual.Count); // the auto-generated "_id_" and our own + compound
+            Assert.Equal(new BsonDocument().Add("OnOne", 1), actual["OnOne_1"].GetElement("key").Value);
+            Assert.Equal(new BsonDocument().Add("OnTwo", 1), actual["OnTwo_1"].GetElement("key").Value);
+        }
+
+        [Fact]
+        public void CreatesMultiLevelCompoundIndexes()
+        {
+            var actual = CreateAndGetIndexes<MultiLevelTwo>();
+
+            Assert.Equal(4, actual.Count); // the auto-generated "_id_" and our own + compound
+            Assert.Equal(new BsonDocument().Add("OnTwo", 1).Add("OnOne", 1), actual["multi_level"].GetElement("key").Value);
+        }
+
         #region Create and get indexes
         private IDictionary<string, BsonDocument> CreateAndGetIndexes<TDocument>()
         {
@@ -87,6 +106,22 @@ namespace RapidCore.Mongo.FunctionalTests
 
             [Index("compound_index", Order = 1)]
             public long CompoundnessOfTheLong { get; set; }
+        }
+        #endregion
+
+        #region Multi level
+        private class MultiLevelOne
+        {
+            [Index]
+            [Index("multi_level")]
+            public string OnOne { get; set; }
+        }
+
+        private class MultiLevelTwo : MultiLevelOne
+        {
+            [Index]
+            [Index("multi_level")]
+            public string OnTwo { get; set; }
         }
         #endregion
     }
