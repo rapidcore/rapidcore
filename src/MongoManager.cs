7c8367fc206f2bac24fcb8a4f249dde771932e27
Patrick Boisen <patrick@skarp.dk>
2017-05-22 09:42:50 +0200

    Use reflection extensions from RapidCore
    Even though the InvokeMethodRecursively (and friends) method does not support taking in a null parameter (for the moment), this still makes the codebase more robust. The regular built-in methods for getting methods, do not search through the inheritance tree, but the new extensions do, which makes our code more resilient to implementations moving around.

--- src/MongoManager.cs
+++ src/MongoManager.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using RapidCore.Mongo.Internal;
 using RapidCore.Reflection;
-using RapidCore.Reflection.Extensions;
 
 namespace RapidCore.Mongo
 {
@@ -25,29 +25,25 @@ namespace RapidCore.Mongo
                 {
                     // make the following call:
                     // lowLevelDb.GetCollection<TDocument>("collectionName").Indexes.CreateOne(index.GetKeySpec(), index.GetOptions())
+
+                    var genericDocType = new Type[] { index.DocumentType };
+
                     var mongoCollection = lowLevelDb
                         .GetType()
-                        .GetTypeInfo()
-                        .GetDeclaredMethod("GetCollection")
-                        .MakeGenericMethod(new Type[] { index.DocumentType })
+                        .GetMethodRecursively("GetCollection", typeof(string), typeof(MongoCollectionSettings))
+                        .MakeGenericMethod(genericDocType)
                         .Invoke(lowLevelDb, new object[] { index.Collection, null });
 
-                    var collectionIndexes = mongoCollection
-                        .GetType()
-                        .GetTypeInfo()
-                        .GetDeclaredProperty("Indexes")
-                        .GetMethod
-                        .Invoke(mongoCollection, new object[0]);
-
-                    var ciType = collectionIndexes.GetType();
-                    var ciTypeInfo = ciType.GetTypeInfo().BaseType.GetTypeInfo();
+                    var collectionIndexes = mongoCollection.InvokeGetterRecursively("Indexes");
 
                     collectionIndexes
                         .GetType()
-                        .GetTypeInfo()
-                        .BaseType // because CreateOne is not declared on MongoIndexManager, but on the base class
-                        .GetTypeInfo()
-                        .GetDeclaredMethod("CreateOne")
+                        .GetMethodRecursively(
+                            "CreateOne",
+                            typeof(IndexKeysDefinition<>).MakeGenericType(genericDocType),
+                            typeof(CreateIndexOptions),
+                            typeof(CancellationToken)
+                        )
                         .Invoke(collectionIndexes, new object[] { index.GetKeySpec(), index.GetOptions(), null });
                 });
             }
