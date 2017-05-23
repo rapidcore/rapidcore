03eb4a7b41291da6e11874f4c7343bba8ed1a73f
Patrick Boisen <patrick@skarp.dk>
2017-05-23 09:21:19 +0200

    Add XML comments

--- src/Internal/IndexDefinition.cs
+++ src/Internal/IndexDefinition.cs
@@ -7,14 +7,41 @@ using MongoDB.Bson;
 
 namespace RapidCore.Mongo.Internal
 {
+    /// <summary>
+    /// Defines an index
+    /// </summary>
     public class IndexDefinition
     {
+        /// <summary>
+        /// The type of the document
+        /// </summary>
         public virtual Type DocumentType { get; set; }
+
+        /// <summary>
+        /// The name of the collection
+        /// </summary>
         public virtual string Collection { get; set; }
+
+        /// <summary>
+        /// The name of the index
+        /// </summary>
         public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Whether or not to make the index sparse
+        /// </summary>
         public virtual bool Sparse { get; set; } = false;
+
+        /// <summary>
+        /// The keys of the index
+        /// </summary>
         public virtual IList<IndexKey> Keys { get; set; } = new List<IndexKey>();
 
+        /// <summary>
+        /// Update this index with the given attribute
+        /// </summary>
+        /// <param name="attribute">The index attribute instance</param>
+        /// <param name="field">The field having the attribute</param>
         public virtual void Update(IndexAttribute attribute, string field)
         {
             if (attribute.Sparse)
@@ -28,13 +55,16 @@ namespace RapidCore.Mongo.Internal
         /// <summary>
         /// Generates an IndexKeysDefinition instance for this index
         /// </summary>
-        /// <returns>BsonDocumentIndexKeysDefinition<DocumentType></returns>
+        /// <returns><c>BsonDocumentIndexKeysDefinition<DocumentType></c></returns>
         public virtual object GetKeySpec()
         {
             var indexDefinitionsType = typeof(BsonDocumentIndexKeysDefinition<>).MakeGenericType(DocumentType);
             return Activator.CreateInstance(indexDefinitionsType, GetKeySpecBsonDocument());
         }
 
+        /// <summary>
+        /// Get options for creating the index
+        /// </summary>
         public virtual CreateIndexOptions GetOptions()
         {
             var options = new CreateIndexOptions();
@@ -46,6 +76,10 @@ namespace RapidCore.Mongo.Internal
             return options;
         }
 
+        /// <summary>
+        /// Generate a <see cref="BsonDocument" /> specifying
+        /// the keys of the index
+        /// </summary>
         private BsonDocument GetKeySpecBsonDocument()
         {
             var doc = new BsonDocument();
