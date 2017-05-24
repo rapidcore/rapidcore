6d334800b560efd98e299f816901bf478d30b6ed
Patrick Boisen <patrick@skarp.dk>
2017-05-24 07:58:05 +0200

    Add EnsureIndexes(.., Type)
    Especially for testing, it is useful to be able to limit what entities will be indexed.

--- src/MongoManager.cs
+++ src/MongoManager.cs
@@ -21,32 +21,42 @@ namespace RapidCore.Mongo
 
             foreach (var type in types)
             {
-                type.GetIndexDefinitions().ToList().ForEach(index =>
-                {
-                    // make the following call:
-                    // lowLevelDb.GetCollection<TDocument>("collectionName").Indexes.CreateOne(index.GetKeySpec(), index.GetOptions())
-
-                    var genericDocType = new Type[] { index.DocumentType };
-
-                    var mongoCollection = lowLevelDb
-                        .GetType()
-                        .GetMethodRecursively("GetCollection", typeof(string), typeof(MongoCollectionSettings))
-                        .MakeGenericMethod(genericDocType)
-                        .Invoke(lowLevelDb, new object[] { index.Collection, null });
-
-                    var collectionIndexes = mongoCollection.InvokeGetterRecursively("Indexes");
-
-                    collectionIndexes
-                        .GetType()
-                        .GetMethodRecursively(
-                            "CreateOne",
-                            typeof(IndexKeysDefinition<>).MakeGenericType(genericDocType),
-                            typeof(CreateIndexOptions),
-                            typeof(CancellationToken)
-                        )
-                        .Invoke(collectionIndexes, new object[] { index.GetKeySpec(), index.GetOptions(), null });
-                });
+                EnsureIndexes(lowLevelDb, type);
             }
+        }
+
+        public void EnsureIndexes(IMongoDatabase lowLevelDb, Type type)
+        {
+            EnsureIndexes(lowLevelDb, type.GetTypeInfo());
+        }
+
+        public void EnsureIndexes(IMongoDatabase lowLevelDb, TypeInfo type)
+        {
+            type.GetIndexDefinitions().ToList().ForEach(index =>
+            {
+                // make the following call:
+                // lowLevelDb.GetCollection<TDocument>("collectionName").Indexes.CreateOne(index.GetKeySpec(), index.GetOptions())
+
+                var genericDocType = new Type[] { index.DocumentType };
+
+                var mongoCollection = lowLevelDb
+                    .GetType()
+                    .GetMethodRecursively("GetCollection", typeof(string), typeof(MongoCollectionSettings))
+                    .MakeGenericMethod(genericDocType)
+                    .Invoke(lowLevelDb, new object[] { index.Collection, null });
+
+                var collectionIndexes = mongoCollection.InvokeGetterRecursively("Indexes");
+
+                collectionIndexes
+                    .GetType()
+                    .GetMethodRecursively(
+                        "CreateOne",
+                        typeof(IndexKeysDefinition<>).MakeGenericType(genericDocType),
+                        typeof(CreateIndexOptions),
+                        typeof(CancellationToken)
+                    )
+                    .Invoke(collectionIndexes, new object[] { index.GetKeySpec(), index.GetOptions(), null });
+            });
         }
     }
 }
