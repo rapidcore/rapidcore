dcbb7feae0aa84cac515fdbb9ecc5ced0829b46e
Patrick Boisen <patrick@skarp.dk>
2017-05-26 15:13:00 +0200

    Add test showing handling of creating twice
    If consumers are supposed to be able to run EnsureIndexes when their app starts, it should not crash if the indexes have not changed.

--- test/functional/MongoManager_EnsureIndexes_Tests.cs
+++ test/functional/MongoManager_EnsureIndexes_Tests.cs
@@ -109,6 +109,17 @@ namespace RapidCore.Mongo.FunctionalTests
             Assert.Equal(new BsonDocument().Add("CompoundnessOfTheLong", 1).Add("StringOfCompoundness", 1), secondSetOfIndexes["compound_index"].GetElement("key").Value);
         }
 
+        [Fact]
+        public void DoNothingIfTheIndexAlreadyExists_andIsTheSame()
+        {
+            CreateAndGetIndexes<SimpleIndexes>(); // should drop and create
+            var afterSecondRun = CreateAndGetIndexes<SimpleIndexes>(false); // should _NOT_ drop
+
+            Assert.Equal(3, afterSecondRun.Count); // the auto-generated "_id_" and our own
+            Assert.Equal(new BsonDocument().Add("String", 1), afterSecondRun["string_index"].GetElement("key").Value);
+            Assert.Equal(new BsonDocument().Add("Int", 1), afterSecondRun["Int_1"].GetElement("key").Value);
+        }
+
         #region Create and get indexes
         private IDictionary<string, BsonDocument> CreateAndGetIndexes<TDocument>(bool doDrop = true)
         {
