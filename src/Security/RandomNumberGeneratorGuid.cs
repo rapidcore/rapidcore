3624a11aff92c861e01a9e45d483447eccb96c21
Rui Martins <rui@skarp.dk>
2017-06-08 09:51:50 +0200

    Implement a guid generator based on 16 random bytes
    The class allows the generation of guids from 16
    cryptographically strong random bytes.

---
+++ src/Security/RandomNumberGeneratorGuid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RapidCore.Security
+{
+    // taken from https://stackoverflow.com/questions/37170388/create-a-cryptographically-secure-random-guid-in-net
+    public class RandomNumberGeneratorGuid
+    {
+        /// <summary>
+        /// Returns a guid created from
+        /// 16 random bytes using a cryptographically strong
+        ///  sequence of random values
+        /// </summary>
+        /// <returns>
+        /// the guid
+        /// </returns>
+        public Guid GenerateGuid()
+        {
+            using (var provider = RandomNumberGenerator.Create())
+            {
+                var bytes = new byte[16];
+                provider.GetBytes(bytes);
+
+                return new Guid(bytes);
+            }
+        }
+    }
+}
---
+++ test/unit/Security/RandomNumberGeneratorGuidTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RapidCore.Security;
+using Xunit;
+
+namespace RapidCore.UnitTests.Security
+{
+    public class RandomNumberGeneratorGuidTest
+    {
+        private readonly RandomNumberGeneratorGuid _guidGenerator;
+
+        public RandomNumberGeneratorGuidTest()
+        {
+            _guidGenerator = new RandomNumberGeneratorGuid();
+        }
+
+        [Fact]
+        public void Can_GenerateGuid()
+        {
+            var guid = _guidGenerator.GenerateGuid();
+
+            Assert.NotNull(guid);
+            Assert.IsType(typeof(Guid), guid);
+            Assert.NotEqual(Guid.Empty, guid);
+        }
+    }
+}
