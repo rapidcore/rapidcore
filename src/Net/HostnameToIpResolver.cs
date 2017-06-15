c1f4413c93429c7fbf662ef27fb07fff2f6cbf8e
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-06-15 14:59:46 +0200

    Add class for resolving hostnames to Ipv4 addresses

---
+++ src/Net/HostnameToIpResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidCore.Net
+{
+    public class HostnameToIpResolver
+    {
+        /// <summary>
+        /// Resolve the given hostname into the first available Ipv4 address
+        /// </summary>
+        /// <param name="hostname">The hostname to resolve</param>
+        /// <returns>The Ipv4 address</returns>
+        public virtual async Task<string> ResolveToIpv4Async(string hostname)
+        {
+            var dnsTask = Dns.GetHostAddressesAsync(hostname);
+            var addresses = await dnsTask;
+            var connect = addresses.Where(x => x.AddressFamily == AddressFamily.InterNetwork).Select(x => x.MapToIPv4().ToString()).First();
+
+            return connect;
+        }
+    }
+}
