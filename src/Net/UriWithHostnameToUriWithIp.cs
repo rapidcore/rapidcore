1d36a84ec1f4943845d21f0f96c8b65afd7a18f8
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-06-15 15:22:51 +0200

    Implement helper to convert uris with hostnames to uris with ips

---
+++ src/Net/UriWithHostnameToUriWithIp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidCore.Net
+{
+    public class UriWithHostnameToUriWithIp
+    {
+        private readonly HostnameToIpResolver resolver;
+
+        public UriWithHostnameToUriWithIp(HostnameToIpResolver resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        /// <summary>
+        /// This extension method takes in a uri that contains a hostname, resolves the ip and returns the correspoding uri with the ip injected
+        ///
+        /// I.e mongodb://my-mongo-server:27017 => mongodb://10.1.1.42:27017
+        ///
+        /// This is to workaround this bug in dotnet core:
+        /// https://github.com/dotnet/corefx/issues/8768
+        /// </summary>
+        /// <param name="uriString">The uri with a hostname to fix</param>
+        /// <returns>The patched uri with an ip instead of a hostname</returns>
+        public async Task<string> ConvertAsync(string uriString)
+        {
+            if (!uriString.Contains("://"))
+            {
+                uriString = $"fakeit://{uriString}";
+            }
+
+            var uri = new Uri(uriString);
+            var ipaddr = await resolver.ResolveToIpv4Async(uri.DnsSafeHost);
+
+            return uri.OriginalString.Replace(uri.Host, ipaddr).Replace("fakeit://", string.Empty);
+        }
+    }
+}
---
+++ test/unit/Net/UriWithHostnameToUriWithIpTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using RapidCore.Net;
+using Xunit;
+
+namespace RapidCore.UnitTests.Net
+{
+    public class UriWithHostnameToUriWithIpTest
+    {
+        private readonly HostnameToIpResolver _resolver;
+
+        public UriWithHostnameToUriWithIpTest()
+        {
+            _resolver = A.Fake<HostnameToIpResolver>();
+            A.CallTo(() => _resolver.ResolveToIpv4Async(A<string>._)).Returns(Task.FromResult("127.0.0.1"));
+        }
+
+        [Theory]
+        [InlineData("mongodb://my-mongo:27017", "mongodb://127.0.0.1:27017")]
+        [InlineData("redis://the-redis:27017", "redis://127.0.0.1:27017")]
+        [InlineData("the-redis:27017", "127.0.0.1:27017")]
+        [InlineData("mongodb://user:pass@my-mongo:27017", "mongodb://user:pass@127.0.0.1:27017")]
+        public async Task HostnameToIp_just_works_async(string toFix, string expectedFix)
+        {
+            var fixer = new UriWithHostnameToUriWithIp(_resolver);
+
+            var fixx = await fixer.ConvertAsync(toFix);
+            Assert.Equal(expectedFix, fixx);
+        }
+    }
+}
