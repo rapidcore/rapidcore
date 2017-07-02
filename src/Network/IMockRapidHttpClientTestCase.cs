2faa4fea942f8805f4efd197849133f5a3313884
Patrick Boisen <patrick@skarp.dk>
2017-07-02 10:28:21 +0200

    Add mockable http client

---
+++ src/Network/IMockRapidHttpClientTestCase.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+
+namespace RapidCore.Network
+{
+    /// <summary>
+    /// Defines the interface for test cases
+    /// used by <see cref="MockRapidHttpClient"/>
+    /// </summary>
+    public interface IMockRapidHttpClientTestCase
+    {
+        /// <summary>
+        /// Is this the request we are looking for?
+        /// </summary>
+        /// <param name="request">The request to check</param>
+        /// <returns><c>True</c> if the request matches this case, <c>false</c> otherwise.</returns>
+        bool IsMatch(HttpRequestMessage request);
+
+        /// <summary>
+        /// Get the mock response for the given request.
+        /// </summary>
+        /// <param name="request">The request to "respond" to</param>
+        /// <returns>The response</returns>
+        HttpResponseMessage GetResponse(HttpRequestMessage request);
+    }
+}
---
+++ src/Network/RealRapidHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RapidCore.Network
+{
+    /// <summary>
+    /// Actually sends request - no mocking.
+    /// </summary>
+    public class RealRapidHttpClient : IRapidHttpClient
+    {
+        private readonly HttpClient httpClient;
+
+        public RealRapidHttpClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Send the request
+        /// </summary>
+        /// <param name="request">The request</param>
+        /// <returns>The response</returns>
+        public virtual Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            return httpClient.SendAsync(request);
+        }
+    }
+}
