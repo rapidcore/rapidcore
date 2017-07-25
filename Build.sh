8d839c2b9c49c4065ba60cb324f30053d80553d7
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-25 13:53:29 +0200

    Fix env tests to not bitch if no env vars are defined

--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Perform the actual build
 dotnet restore \
 && dotnet build -c Release -f netstandard1.6 src/rapidcore.csproj \
 && dotnet test test/unit/unittests.csproj -c Release -f netcoreapp1.1
