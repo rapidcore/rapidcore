b86becbac93a40ff4142d94afb17f2814c583577
Patrick Boisen <patrick@skarp.dk>
2017-08-07 14:24:07 +0200

    Implement IMigrationEnvironment

---
+++ test/unit/Migration/MigrationEnvironmentTests.cs
@@ -0,0 +1,32 @@
+﻿using RapidCore.Mongo.Migration;
+using Xunit;
+
+namespace RapidCore.Mongo.UnitTests.Migration
+{
+    public class MigrationEnvironmentTests
+    {
+        [Fact]
+        public void IsTesting_works_caseInsensitive()
+        {
+            Assert.True(new MigrationEnvironment("teSTiNg").IsTesting());
+        }
+        
+        [Fact]
+        public void IsStaging_works_caseInsensitive()
+        {
+            Assert.True(new MigrationEnvironment("STAging").IsStaging());
+        }
+        
+        [Fact]
+        public void IsProduction_works_caseInsensitive()
+        {
+            Assert.True(new MigrationEnvironment("ProDUCtion").IsProduction());
+        }
+        
+        [Fact]
+        public void Environment_Works()
+        {
+            Assert.Equal("das environment", new MigrationEnvironment("das environment").Environment);
+        }
+    }
+}
