66d97592651ea685c1c72059cdc3b558b523e09a
Patrick Boisen <patrick@skarp.dk>
2017-08-07 16:13:13 +0200

    WIP full stack test migration

---
+++ test/functional/Migration/MigrationTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using FakeItEasy;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RapidCore.Locking;
+using RapidCore.Mongo.Migration;
+using RapidCore.Mongo.Testing;
+using ServiceStack;
+using Xunit;
+
+namespace RapidCore.Mongo.FunctionalTests.Migration
+{
+    public class MigrationTests : MongoConnectedTestBase
+    {
+        [Fact]
+        public async void RunMigration()
+        {
+            var services = new ServiceCollection();
+            var connectionProvider = new ConnectionProvider();
+            
+            var runner = new MigrationRunner(
+                new LoggerFactory().CreateLogger<MigrationRunner>(),
+                new ServiceProviderContainerAdapter(services.BuildServiceProvider()), 
+                new MigrationEnvironment("staging"), 
+                connectionProvider,
+                new MigrationManager(new List<Assembly> {typeof(MigrationTests).GetAssembly()}),
+                A.Fake<IDistributedAppLockProvider>()
+            );
+            
+            // TODO setup some state
+
+            await runner.UpgradeAsync();
+            
+            // TODO make some checks
+        }
+    }
+}
---
+++ test/functional/Migration/TestMigration/Migration01.cs
@@ -0,0 +1,18 @@
+﻿using RapidCore.Mongo.Migration;
+using RapidCore.Mongo.Migration.Internal;
+
+namespace RapidCore.Mongo.FunctionalTests.Migration.TestMigration
+{
+    public class Migration01 : MigrationBase
+    {
+        public override void ConfigureUpgrade(IMigrationBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ConfigureDowngrade(IMigrationBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
