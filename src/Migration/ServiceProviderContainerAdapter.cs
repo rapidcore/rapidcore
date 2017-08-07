c4d0612a333cc171058457cc7897f57132ad7ace
Patrick Boisen <patrick@skarp.dk>
2017-08-07 14:44:18 +0200

    Implement IContainerAdapter for IServiceProvider

---
+++ src/Migration/ServiceProviderContainerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RapidCore.Mongo.Migration
+{
+    /// <summary>
+    /// Container adapter for <see cref="IServiceProvider"/>
+    /// </summary>
+    public class ServiceProviderContainerAdapter : IContainerAdapter
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ServiceProviderContainerAdapter(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public T Resolve<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public T Resolve<T>(string name)
+        {
+            // named instances does not exist in IServiceProvider
+            // so just do a normal resolve
+            return Resolve<T>();
+        }
+    }
+}
