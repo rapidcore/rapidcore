5277bb1e4579dd7920cef7c100155201d5383ab3
Patrick Boisen <patrick@skarp.dk>
2017-08-09 08:04:23 +0200

    Add NoopDistributedAppLock-stack

---
+++ src/Locking/NoopDistributedAppLock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RapidCore.Locking
+{
+    /// <summary>
+    /// <see cref="NoopDistributedAppLockProvider"/>
+    /// </summary>
+    [Obsolete("This should not be used in real systems", false)]
+    public class NoopDistributedAppLock : IDistributedAppLock
+    {
+        public void Dispose()
+        {
+            IsActive = false;
+        }
+
+        public string Name { get; set; }
+        
+        public bool IsActive { get; set; }
+        
+        /// <summary>
+        /// Determines whether the current lock instance is <see cref="IsActive"/> and has a name that matches the given
+        /// parameter
+        /// </summary>
+        /// <param name="name">The name of the lock to asssert that is currently taken</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public void ThrowIfNotActiveWithGivenName(string name)
+        {
+            if (!IsActive)
+            {
+                throw new InvalidOperationException(
+                    $"Lock precondition mismatch, required IsActive=true with name '{name}' but IsActive=false with name '{this.Name}'");
+            }
+
+            if (!Name.Equals(name))
+            {
+                throw new InvalidOperationException(
+                    $"Lock precondition mismatch, required IsActive=true with name '{name}' but IsActive=true with name '{this.Name}'");
+            }
+        }
+    }
+}
---
+++ src/Locking/NoopDistributedAppLockProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using RapidCore.Threading;
+
+namespace RapidCore.Locking
+{
+    /// <summary>
+    /// Provides an easy-to-get-started distributed app locker. Note though
+    /// that IT DOES NOT LOCK ANYTHING.
+    /// 
+    /// It is meant to help you get started on coding while you figure out / wait for
+    /// servers to be available (in this case, maybe check out https://docker.com).
+    /// </summary>
+    [Obsolete("This should not be used in real systems", false)]
+    public class NoopDistributedAppLockProvider : IDistributedAppLockProvider
+    {
+        public virtual IDistributedAppLock Acquire(string lockName, TimeSpan? lockWaitTimeout = null)
+        {
+            return AcquireAsync(lockName, lockWaitTimeout).AwaitSync();
+        }
+
+        public virtual async Task<IDistributedAppLock> AcquireAsync(string lockName, TimeSpan? lockWaitTimeout = null)
+        {
+            return await Task.FromResult(new NoopDistributedAppLock
+            {
+                IsActive = true,
+                Name = lockName
+            });
+        }
+    }
+}
