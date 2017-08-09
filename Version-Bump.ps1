f1761301351215539dcd109af428ac49a275dd2e
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-08-09 15:24:49 +0200

    Add strategy as required parameter and VALIDATE it

--- Version-Bump.ps1
+++ Version-Bump.ps1
@@ -1,3 +1,12 @@
+# The user must specify an -updatestrategy when invoking the script
+param ($UpdateStrategy = $(throw "Update strategy parameter is required!"))   
+
+# Check that it's an allowed value
+$validStrategy = $UpdateStrategy -match "^major|minor|patch$"
+if ($validStrategy -ne $true) {
+    throw "Invalid update strategy, must be one of: major, minor, patch"
+}
+
 function Update-Clone {
     # $path: the path to navigate to, where we should update versions and all that
     # $goTo: where to navigate after we are done
@@ -5,7 +14,7 @@ function Update-Clone {
     # $updatePackageRef: whether we should update the RapidCore package reference - should be false when operating on rapidcore it self.
     Param ([string]$path, [string]$goTo, [string]$version, [bool]$updatePackageRef)
     
-    Write-Host "updating " $path 
+    Write-Host "updating " $path -ForegroundColor Blue
     Set-Location $path
     $output = (& git checkout master 2>&1)
     $output = (& git pull 2>&1)
@@ -26,12 +35,11 @@ function Update-Clone {
     return $newVersion
 }
 
-$updateStrategy = $args[0]
-Write-Host "Update strategy: " $updateStrategy
+Write-Host "Update strategy: " $UpdateStrategy -ForegroundColor Green
 
 # Go to the rapidcore repo and update
-$newVersion = Update-Clone -path .\rapidcore\src -goTo ..\..\ -version $updateStrategy -updatePackageRef $false
-Write-Host "Version of rapidcore is: " $newVersion " setting this explicit version on all other rapidcore libs"
+$newVersion = Update-Clone -path .\rapidcore\src -goTo ..\..\ -version $UpdateStrategy -updatePackageRef $false
+Write-Host "Version of rapidcore is: " $newVersion " setting this explicit version on all other rapidcore libs" -ForegroundColor Green
 
 Update-Clone -path .\rapidcore.mongo\src -goTo ..\..\ -version $newVersion -updatePackageRef $true |Out-Null
 Update-Clone -path .\rapidcore.redis\src -goTo ..\..\ -version $newVersion -updatePackageRef $true |Out-Null
