e551e3e4a91445f74999fb2eff1e85e8d36a8eb8
Patrick Boisen <patrick@skarp.dk>
2017-08-14 15:29:30 +0200

    Add IMigrationContext

---
+++ src/Migration/IMigrationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using RapidCore.DependencyInjection;
+
+namespace RapidCore.Migration
+{
+    public interface IMigrationContext
+    {
+        ILogger Logger { get; set; }
+        
+        IRapidContainerAdapter Container { get; set; }
+        
+        IMigrationEnvironment Environment { get; set; }
+    }
+}
