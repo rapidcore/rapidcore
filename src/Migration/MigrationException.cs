8eb4089d11d95e603cc1b657a6ee1a19bdf70c4d
Patrick Boisen <patrick@skarp.dk>
2017-08-14 15:30:19 +0200

    Add MigrationException

---
+++ src/Migration/MigrationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RapidCore.Migration
+{
+    public class MigrationException : Exception
+    {
+        public MigrationException(string message) : base(message)
+        {
+        }
+
+        public MigrationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
