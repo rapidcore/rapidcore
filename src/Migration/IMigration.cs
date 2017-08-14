9eca42200a4bcadf327818b90c6a5e674591e058
Patrick Boisen <patrick@skarp.dk>
2017-08-14 15:31:45 +0200

    Add interface for a migration

---
+++ src/Migration/IMigration.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace RapidCore.Migration
+{
+    /// <summary>
+    /// Defines a migration
+    /// </summary>
+    public interface IMigration
+    {
+        /// <summary>
+        /// Code that is run when upgrading the environment
+        /// </summary>
+        Task UpgradeAsync(IMigrationContext context);
+        
+        /// <summary>
+        /// Code that is run when downgrading (or rolling back) the environment
+        /// </summary>
+        Task DowngradeAsync(IMigrationContext context);
+
+        string Name { get; }
+    }
+}
