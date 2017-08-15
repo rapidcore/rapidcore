1fee1fb3b985ca67471a3baa615a71ade526f7d2
Patrick Boisen <patrick@skarp.dk>
2017-08-15 10:56:19 +0200

    Add MigrationRunner

---
+++ src/Migration/MigrationRunner.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using RapidCore.DependencyInjection;
+using RapidCore.Locking;
+
+namespace RapidCore.Migration
+{
+    /// <summary>
+    /// Runs migrations
+    /// </summary>
+    public class MigrationRunner
+    {
+        protected readonly ILogger<MigrationRunner> logger;
+        protected readonly IRapidContainerAdapter container;
+        protected readonly IMigrationEnvironment environment;
+        protected readonly IMigrationManager migrationManager;
+        protected readonly IDistributedAppLockProvider appLocker;
+        protected readonly IMigrationContextFactory contextFactory;
+
+        public MigrationRunner(
+            ILogger<MigrationRunner> logger,
+            IRapidContainerAdapter container,
+            IMigrationEnvironment environment,
+            IMigrationManager migrationManager,
+            IDistributedAppLockProvider appLocker,
+            IMigrationContextFactory contextFactory
+        )
+        {
+            this.logger = logger;
+            this.container = container;
+            this.environment = environment;
+            this.migrationManager = migrationManager;
+            this.appLocker = appLocker;
+            this.contextFactory = contextFactory;
+        }
+
+        protected virtual string GetLockName()
+        {
+            return "RapidCoreMigrations";
+        }
+
+        protected virtual IMigrationContext GetContext()
+        {
+            var ctx = contextFactory.GetContext();
+            ctx.Logger = logger;
+            ctx.Environment = environment;
+            ctx.Container = container;
+
+            return ctx;
+        }
+
+        /// <summary>
+        /// Upgrade the enviroment
+        /// </summary>
+        public virtual async Task UpgradeAsync()
+        {
+            // 1. Ensure no one else runs migrations (i.e. lock)
+            // 2. Find migrations
+            // 3. Run each migration
+            // 4. Mark successfully run migrations as completed
+            
+            using (await appLocker.AcquireAsync(GetLockName(), TimeSpan.FromSeconds(30)))
+            {
+                logger.LogInformation($"Lock {GetLockName()} acquired");
+                var sw = new Stopwatch();
+
+                var context = GetContext();
+                
+                foreach (var migration in await migrationManager.FindMigrationsForUpgradeAsync(context))
+                {
+                    try
+                    {
+                        logger.LogInformation($"Attempt to run migration {migration.Name}");
+                        sw.Restart();
+                        await migration.UpgradeAsync(context);
+                        sw.Stop();
+                        logger.LogInformation($"Succeeded in running {migration.Name}. It took {sw.ElapsedMilliseconds} milliseconds.");
+                        
+                        await migrationManager.MarkAsCompleteAsync(migration, sw.ElapsedMilliseconds, context);
+                    }
+                    catch (Exception ex)
+                    {
+                        sw.Stop();
+                        logger.LogCritical(666, ex, $"Failed to run migration {migration.Name} after {sw.ElapsedMilliseconds} milliseconds.");
+                        throw new MigrationException($"Failed to run migration {migration.Name}. See inner exception.", ex);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Downgrade the enviroment
+        /// </summary>
+        public virtual async Task DowngradeAsync()
+        {
+            await Task.FromException(new NotImplementedException("Downgrade has not been implemented yet. Provide your input in https://github.com/rapidcore/issues/issues/14"));
+        }
+    }
+}
---
+++ test/unit/Migration/MigrationRunnerTests/UpgradeAsyncTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using RapidCore.DependencyInjection;
+using RapidCore.Locking;
+using RapidCore.Migration;
+using Xunit;
+
+namespace RapidCore.UnitTests.Migration.MigrationRunnerTests
+{
+    public class UpgradeAsyncTests
+    {
+        private readonly MigrationRunner runner;
+        private readonly ILogger<MigrationRunner> logger;
+        private readonly IRapidContainerAdapter container;
+        private readonly IMigrationEnvironment environment;
+        private readonly IMigrationManager migrationManager;
+        private readonly IDistributedAppLockProvider appLocker;
+        private readonly IMigrationContextFactory contextFactory;
+        private readonly IMigrationContext context;
+        private readonly IDistributedAppLock appLock;
+        private readonly IMigration migration1;
+        private readonly IMigration migration2;
+
+        public UpgradeAsyncTests()
+        {
+            logger = A.Fake<ILogger<MigrationRunner>>();
+            container = A.Fake<IRapidContainerAdapter>();
+            environment = A.Fake<IMigrationEnvironment>();
+            migrationManager = A.Fake<IMigrationManager>();
+            appLocker = A.Fake<IDistributedAppLockProvider>();
+            contextFactory = A.Fake<IMigrationContextFactory>();
+            context = A.Fake<IMigrationContext>();
+            appLock = A.Fake<IDistributedAppLock>();
+            migration1 = A.Fake<IMigration>();
+            migration2 = A.Fake<IMigration>();
+
+            A.CallTo(() => appLocker.AcquireAsync("RapidCoreMigrations", A<TimeSpan>._)).Returns(Task.FromResult(appLock));
+            A.CallTo(() => contextFactory.GetContext()).Returns(context);
+            
+            runner = new MigrationRunner(
+                logger,
+                container,
+                environment,
+                migrationManager,
+                appLocker,
+                contextFactory
+            );
+        }
+
+        [Fact]
+        public async void Upgrade_Locks()
+        {
+            await runner.UpgradeAsync();
+
+            A.CallTo(() => appLocker.AcquireAsync("RapidCoreMigrations", A<TimeSpan>.That.Matches(x => x == TimeSpan.FromSeconds(30)))).MustHaveHappened();
+        }
+
+        [Fact]
+        public async void Upgrade_SetsLoggerEtcOnContext()
+        {
+            await runner.UpgradeAsync();
+
+            A.CallToSet(() => context.Container).To(container).MustHaveHappened();
+            A.CallToSet(() => context.Environment).To(environment).MustHaveHappened();
+            A.CallToSet(() => context.Logger).To(logger).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void Upgrade_GetMigrationsFromManager()
+        {
+            await runner.UpgradeAsync();
+
+            A.CallTo(() => migrationManager.FindMigrationsForUpgradeAsync(context)).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void Upgrade_CallUpgradeOnAllMigrations()
+        {
+            A.CallTo(() => migrationManager.FindMigrationsForUpgradeAsync(A<IMigrationContext>._)).Returns(Task.FromResult<IList<IMigration>>(new List<IMigration> {migration1, migration2}));
+            
+            await runner.UpgradeAsync();
+
+            A.CallTo(() => migration1.UpgradeAsync(context)).MustHaveHappened();
+            A.CallTo(() => migration2.UpgradeAsync(context)).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void Upgrade_MarkMigrationAsComplete()
+        {
+            A.CallTo(() => migrationManager.FindMigrationsForUpgradeAsync(A<IMigrationContext>._)).Returns(Task.FromResult<IList<IMigration>>(new List<IMigration> {migration1}));
+            
+            await runner.UpgradeAsync();
+
+            A.CallTo(() => migrationManager.MarkAsCompleteAsync(migration1, A<long>._, context)).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void Upgrade_ifError_doNotMarkAsComplete_wrapThrownException()
+        {
+            var innerException = new Exception("DIE!");
+            A.CallTo(() => migrationManager.FindMigrationsForUpgradeAsync(A<IMigrationContext>._)).Returns(Task.FromResult<IList<IMigration>>(new List<IMigration> {migration1}));
+            A.CallTo(() => migration1.UpgradeAsync(A<IMigrationContext>._)).ThrowsAsync(innerException);
+
+            var actual = await Record.ExceptionAsync(async () => await runner.UpgradeAsync());
+            
+            Assert.NotNull(actual);
+            Assert.Same(innerException, actual.InnerException);
+            A.CallTo(() => migrationManager.MarkAsCompleteAsync(A<IMigration>._, A<long>._, A<IMigrationContext>._)).MustNotHaveHappened();
+        }
+    }
+}
