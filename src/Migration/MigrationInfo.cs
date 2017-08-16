bde64e3dd125dacadd30e60f52f2d3ff541f44c2
Patrick Boisen <patrick@skarp.dk>
2017-08-16 11:32:46 +0200

    Add "virtual" + AddCompletedStep
    If a consumer is to add a completed step, he should not have to know how MigrationInfo stores steps internally - hence he should be able to simply tell the MigrationInfo that "I have completed this step". It also makes it easier to unit test.

--- src/Migration/MigrationInfo.cs
+++ src/Migration/MigrationInfo.cs
@@ -19,22 +19,31 @@ namespace RapidCore.Migration
         /// <summary>
         /// Gets or sets the list of steps that have been successfully completed.
         /// </summary>
-        public List<string> StepsCompleted { get; set; } = new List<string>();
+        public virtual List<string> StepsCompleted { get; set; } = new List<string>();
 
         /// <summary>
         /// Gets or sets a value indicating whether the migration has
         /// been completed (i.e. all steps have run successfully).
         /// </summary>
-        public bool MigrationCompleted { get; set; }
+        public virtual bool MigrationCompleted { get; set; }
 
         /// <summary>
         /// Gets or sets the total migration time in milliseconds
         /// </summary>
-        public long TotalMigrationTimeInMs { get; set; }
+        public virtual long TotalMigrationTimeInMs { get; set; }
 
         /// <summary>
         /// Gets or sets the completed at date and time in UTC.
         /// </summary>
-        public DateTime CompletedAtUtc { get; set; }
+        public virtual DateTime CompletedAtUtc { get; set; }
+
+        /// <summary>
+        /// Add a completed step
+        /// </summary>
+        /// <param name="stepName">The name of the completed step</param>
+        public virtual void AddCompletedStep(string stepName)
+        {
+            StepsCompleted.Add(stepName);
+        }
     }
 }
