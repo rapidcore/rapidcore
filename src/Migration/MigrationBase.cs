6ac15eb5c6872adb1f725fe6d8e4cd7be46330e7
Patrick Boisen <patrick@skarp.dk>
2017-08-16 11:33:31 +0200

    Add MigrationBase

---
+++ src/Migration/MigrationBase.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RapidCore.Migration.Internal;
+
+namespace RapidCore.Migration
+{
+    public abstract class MigrationBase : IMigration
+    {
+        protected MigrationBase()
+        {
+
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Apply this upgrade migration to the underlying database
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public virtual async Task UpgradeAsync(IMigrationContext context)
+        {
+            this.Context = context;
+
+            var builder = new MigrationBuilder();
+            ConfigureUpgrade(builder);
+            
+            (var stepsToApply, var info) = await GetPendingStepsAsync(builder);
+
+            info.Name = Name; // in case this is a new MigrationInfo
+
+            foreach (var step in stepsToApply)
+            {
+                // Retrieve the migration action
+                var actionForMigrationStep = builder.GetActionForMigrationStep(step);
+                
+                // Run the action
+                await actionForMigrationStep();
+
+                // Add the completion to storage to ensure that we can re-enter if later steps fail
+                info.AddCompletedStep(step);
+                await UpsertMigrationInfoAsync(info);
+            }
+        }
+
+        /// <summary>
+        /// When implemented in a downstream migration it will configure the steps required for the upgrade migration
+        /// </summary>
+        /// <param name="builder"></param>
+        protected abstract void ConfigureUpgrade(IMigrationBuilder builder);
+
+        /// <summary>
+        /// Apply a migration downgrade
+        /// </summary>
+        /// <param name="context"></param>
+        public virtual Task DowngradeAsync(IMigrationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// When implemented in a downstream migration it will configure the steps required for the DOWN migration
+        /// </summary>
+        /// <param name="builder"></param>
+        protected abstract void ConfigureDowngrade(IMigrationBuilder builder);
+
+        #region Helper methods
+        /// <summary>
+        /// Calculate which steps are currently pending from this migration
+        /// </summary>
+        /// <remarks>
+        /// The underlying storage will be queried to return the stored migration information on which the already executed steps are recorded.
+        /// 
+        /// In cases where this is the first time the migration is executed, no document will be stored and thus all steps should be applied
+        /// </remarks>
+        /// <param name="builder">The migration builder instance</param>
+        /// <returns>A list of the pending steps as well as an instantiataed <see cref="MigrationInfo"/></returns>
+        public virtual async Task<(IList<string>, MigrationInfo)> GetPendingStepsAsync(MigrationBuilder builder)
+        {
+            var allStepNames = builder.GetAllStepNames();
+
+            var info = await GetMigrationInfoAsync();
+            
+            if (info != null)
+            {
+                var filteredSteps = allStepNames.Except(info.StepsCompleted).ToList();
+                return (filteredSteps, info);
+            }
+            return (allStepNames, new MigrationInfo());
+        }
+
+        /// <summary>
+        /// Get the <see cref="MigrationInfo"/> instance for this migration
+        /// or <c>null</c> if such an instance does not exist yet.
+        /// 
+        /// Tip to implementors: use <see cref="Context"/> and <see cref="Name"/>.
+        /// </summary>
+        protected abstract Task<MigrationInfo> GetMigrationInfoAsync();
+
+        /// <summary>
+        /// Save (insert or update) the given <paramref name="info"/>
+        /// </summary>
+        /// <param name="info">The migration info to save</param>
+        protected abstract Task UpsertMigrationInfoAsync(MigrationInfo info);
+        #endregion Helper methods
+
+        #region props
+        /// <summary>
+        /// Get the name of this migration
+        /// </summary>
+        public virtual string Name => GetType().Name;
+
+        /// <summary>
+        /// The migration context injected when initiating the up or down phase of the migration
+        /// </summary>
+        protected IMigrationContext Context { get; set; }
+        #endregion
+    }
+}
---
+++ test/unit/Migration/MigrationBaseTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FakeItEasy;
+using RapidCore.Migration;
+using RapidCore.Migration.Internal;
+using Xunit;
+
+namespace RapidCore.UnitTests.Migration
+{
+    public class MigrationBaseTest
+    {
+        private readonly ImplMigrationBase migration;
+        private readonly IMigrationContext context;
+        private readonly MigrationInfo info;
+
+        public MigrationBaseTest()
+        {
+            context = A.Fake<IMigrationContext>();
+            info = A.Fake<MigrationInfo>();
+
+            migration = new ImplMigrationBase
+            {
+                ConfigureUpgradeAction = A.Fake<Action<IMigrationBuilder>>(),
+                ConfigureDowngradeAction = A.Fake<Action<IMigrationBuilder>>(),
+                GetMigrationInfoFunc = A.Fake<Func<MigrationInfo>>(),
+                UpsertMigrationInfoAction = A.Fake<Action<MigrationInfo>>()
+            };
+        }
+
+        #region UpgradeAsync
+        [Fact]
+        public async void UpgradeAsync_callsConfigureWithBuilder()
+        {
+            await migration.UpgradeAsync(context);
+
+            A.CallTo(() => migration.ConfigureUpgradeAction(A<IMigrationBuilder>._)).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void UpgradeAsync_setsTheMigrationNameOnTheInfo()
+        {
+            A.CallTo(() => migration.GetMigrationInfoFunc.Invoke()).Returns(info);
+            
+            await migration.UpgradeAsync(context);
+
+            A.CallToSet(() => info.Name).To(nameof(ImplMigrationBase)).MustHaveHappened();
+        }
+        
+        [Fact]
+        public async void UpgradeAsync_applyAndSaveEachStep_inSequence()
+        {
+            var step1Action = A.Fake<Action>();
+            var step2Action = A.Fake<Action>();
+            
+            A.CallTo(() => migration.ConfigureUpgradeAction.Invoke(A<IMigrationBuilder>._)).Invokes((IMigrationBuilder builder) =>
+            {
+                builder.Step("one", step1Action);
+                builder.Step("two", step2Action);
+            });
+            
+            A.CallTo(() => migration.GetMigrationInfoFunc.Invoke()).Returns(info);
+            
+            await migration.UpgradeAsync(context);
+
+            A.CallTo(() => step1Action.Invoke()).MustHaveHappened()
+                .Then(A.CallTo(() => info.AddCompletedStep("one")).MustHaveHappened())
+                .Then(A.CallTo(() => migration.UpsertMigrationInfoAction(info)).MustHaveHappened())
+                .Then(A.CallTo(() => step2Action.Invoke()).MustHaveHappened())
+                .Then(A.CallTo(() => info.AddCompletedStep("two")).MustHaveHappened())
+                .Then(A.CallTo(() => migration.UpsertMigrationInfoAction(info)).MustHaveHappened());
+        }
+        #endregion
+
+        #region GetPendingStepsAsync
+        [Fact]
+        public async void GetPendingStepsAsync_ifNoMigrationInfo_returnsAllSteps_and_newInfo()
+        {
+            var expectedAllSteps = new List<string> {"one", "two"};
+            var builder = A.Fake<MigrationBuilder>();
+            A.CallTo(() => builder.GetAllStepNames()).Returns(expectedAllSteps);
+            
+            A.CallTo(() => migration.GetMigrationInfoFunc.Invoke()).Returns(null);
+            
+            (var actualSteps, var actualInfo) = await migration.GetPendingStepsAsync(builder); 
+
+            Assert.Same(expectedAllSteps, actualSteps);
+            Assert.NotNull(actualInfo);
+        }
+        
+        [Fact]
+        public async void GetPendingStepsAsync_returnsFilteredSteps_andExistingInfo()
+        {
+            var allSteps = new List<string> {"one", "two", "three"};
+            var builder = A.Fake<MigrationBuilder>();
+            A.CallTo(() => builder.GetAllStepNames()).Returns(allSteps);
+            
+            A.CallTo(() => migration.GetMigrationInfoFunc.Invoke()).Returns(info);
+            A.CallTo(() => info.StepsCompleted).Returns(new List<string> {"one", "two"});
+            
+            (var actualSteps, var actualInfo) = await migration.GetPendingStepsAsync(builder); 
+
+            Assert.Equal(new List<string> {"three"}, actualSteps);
+            Assert.Same(info, actualInfo);
+        }
+        #endregion
+
+        #region Name
+        [Fact]
+        public void Name_returnsNameOfTheClass()
+        {
+            Assert.Equal("ImplMigrationBase", migration.Name);
+        }
+        #endregion
+        
+        #region Implementation
+        private class ImplMigrationBase : MigrationBase
+        {
+            public Action<IMigrationBuilder> ConfigureUpgradeAction { get; set; }
+            public Action<IMigrationBuilder> ConfigureDowngradeAction { get; set; }
+            public Func<MigrationInfo> GetMigrationInfoFunc { get; set; }
+            public Action<MigrationInfo> UpsertMigrationInfoAction { get; set; }
+            
+            protected override void ConfigureUpgrade(IMigrationBuilder builder)
+            {
+                ConfigureUpgradeAction(builder);
+            }
+
+            protected override void ConfigureDowngrade(IMigrationBuilder builder)
+            {
+                ConfigureDowngradeAction(builder);
+            }
+
+            protected override async Task<MigrationInfo> GetMigrationInfoAsync()
+            {
+                return await Task.FromResult(GetMigrationInfoFunc());
+            }
+
+            protected override async Task UpsertMigrationInfoAsync(MigrationInfo info)
+            {
+                UpsertMigrationInfoAction(info);
+                await Task.CompletedTask;
+            }
+        }
+        #endregion
+    }
+}
