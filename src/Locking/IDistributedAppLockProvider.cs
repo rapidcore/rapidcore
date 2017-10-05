c902d7e95f6ba01cbda44ba54f6ac06492811c32
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-10-05 12:53:50 +0200

    Add lockAutoExpireTimeout to IDistributedAppLock Acquire methods
    The purpose of this property, if set, is that implemtors of distributed
    app locks shall ask the backing store to expire lock keys after the given
    interval, or otherwise implement functionality that will "wipe" leftover
    keys after the given interval has expired.

--- src/Locking/IDistributedAppLockProvider.cs
+++ src/Locking/IDistributedAppLockProvider.cs
@@ -14,22 +14,28 @@ namespace RapidCore.Locking
         ///
         /// If lockWaitTimeout is defined it will wait / retry for the given amount of time before throwing an exception
         /// and giving up on acquiring the lock
+        /// 
+        /// Providing a <paramref name="lockAutoExpireTimeout"/> will enable the distributed app lock provider to automatically
+        /// release stale locks in case the application has crashed and didn't release the lock
         /// </summary>
         /// <exception cref="DistributedAppLockException">Thrown if the lock cannot be acquired</exception>
         /// <param name="lockName">The name of the lock to acquire</param>
         /// <param name="lockWaitTimeout">The time to wait / retry acquiring the lock</param>
-        IDistributedAppLock Acquire(string lockName, TimeSpan? lockWaitTimeout = null);
+        /// <param name="lockAutoExpireTimeout">The amount of time before the lock should automatically be expired</param>
+        IDistributedAppLock Acquire(string lockName, TimeSpan? lockWaitTimeout = null, TimeSpan? lockAutoExpireTimeout = null);
 
-        /// <summary>
-        /// When implemented in a downstream lock class, it will try to acquire the lock in a non-blocking (async) manner
-        ///
-        /// If lockWaitTimeout is defined it will wait / retry for the given amount of time before throwing an exception
-        /// and giving up on acquiring the lock
-        /// </summary>
-        /// <exception cref="DistributedAppLockException">Thrown if the lock cannot be acquired</exception>
-        /// <param name="lockName">The name of the lock to acquire</param>
-        /// <param name="lockWaitTimeout">The time to wait / retry acquiring the lock</param>
+        ///  <summary>
+        ///  When implemented in a downstream lock class, it will try to acquire the lock in a non-blocking (async) manner
+        ///  If lockWaitTimeout is defined it will wait / retry for the given amount of time before throwing an exception
+        ///  and giving up on acquiring the lock
+        /// Providing a <paramref name="lockAutoExpireTimeout"/> will enable the distributed app lock provider to automatically
+        /// release stale locks in case the application has crashed and didn't release the lock
+        ///  </summary>
+        ///  <exception cref="DistributedAppLockException">Thrown if the lock cannot be acquired</exception>
+        ///  <param name="lockName">The name of the lock to acquire</param>
+        ///  <param name="lockWaitTimeout">The time to wait / retry acquiring the lock</param>
+        /// <param name="lockAutoExpireTimeout">The amount of time before the lock should automatically be expired</param>
         /// <returns></returns>
-        Task<IDistributedAppLock> AcquireAsync(string lockName, TimeSpan? lockWaitTimeout = null);
+        Task<IDistributedAppLock> AcquireAsync(string lockName, TimeSpan? lockWaitTimeout = null, TimeSpan? lockAutoExpireTimeout = null);
     }
 }
