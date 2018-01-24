fef06a33174570cd15d8d6cb6b39ce05fa41e29e
Patrick Boisen <patrick@skarp.dk>
2018-01-24 18:55:58 +0100

    Build in release mode

--- Build.ps1
+++ Build.ps1
@@ -40,7 +40,7 @@ exec { & dotnet restore }
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet build }
+exec { & dotnet build -c Release }
 
 exec { & dotnet test '.\src\core\test-unit\unittests.csproj' -c Release }
 exec { & dotnet test '.\src\mongo\test-unit\unittests.csproj' -c Release }
