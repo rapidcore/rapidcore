0b4d18d07d7a4632c0057a192a59960e686af9fc
Patrick Boisen <patrick@skarp.dk>
2018-01-24 18:56:16 +0100

    Removed unused code

--- Build.ps1
+++ Build.ps1
@@ -37,9 +37,6 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
 exec { & dotnet build -c Release }
 
 exec { & dotnet test '.\src\core\test-unit\unittests.csproj' -c Release }
