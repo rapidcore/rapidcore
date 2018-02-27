24a008959a6d66c65ed0ad4231f3e1280146a468
Rui Martins <rui@skarp.dk>
2018-02-27 13:49:15 +0100

    Implement MigrationStorage raw queries

--- src/postgresql/main/PostgreSqlMigrationStorage.cs
+++ src/postgresql/main/PostgreSqlMigrationStorage.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 using Dapper;
 using RapidCore.Migration;
 
-namespace rapidcore.postgresql
+namespace Rapidcore.Postgresql
 {
     public class PostgreSqlMigrationStorage : IMigrationStorage
     {
-        public Task MarkAsCompleteAsync(IMigrationContext context, IMigration migration, long milliseconds)
+        public async Task MarkAsCompleteAsync(IMigrationContext context, IMigration migration, long milliseconds)
         {
-            var db = GetDb(context);
-            return null;
+            var info = await GetMigrationInfoAsync(context, migration.Name);
+            if (info == default(MigrationInfo))
+            {
+                info = new MigrationInfo
+                {
+                    Name = migration.Name
+                };
+            }
+
+            info.CompletedAtUtc = DateTime.UtcNow;
+            info.MigrationCompleted = true;
+            info.TotalMigrationTimeInMs = milliseconds;
+
+            await UpsertMigrationInfoAsync(context, info);
         }
 
         private IDbConnection GetDb(IMigrationContext context)
@@ -24,18 +36,74 @@ namespace rapidcore.postgresql
         public async Task<MigrationInfo> GetMigrationInfoAsync(IMigrationContext context, string migrationName)
         {
             var db = GetDb(context);
-            var migrationInfo = await db.QuerySingleAsync<MigrationInfo>("select Name = @MigrationName", new { MigrationName = migrationName });
+            var migrationInfo = await db.QuerySingleOrDefaultAsync<MigrationInfo>("select * from MigrationInfo where Name = @MigrationName", new { MigrationName = migrationName });
             return migrationInfo;
         }
 
-        public Task UpsertMigrationInfoAsync(IMigrationContext context, MigrationInfo info)
+        public async Task UpsertMigrationInfoAsync(IMigrationContext context, MigrationInfo info)
         {
-            throw new NotImplementedException();
+            // TODO: steps completed
+            // TODO: support update
+            var db = GetDb(context);
+            if (info.Id != null)
+            {
+                int id = Convert.ToInt32(info.Id);
+                string updateQuery = @"UPDATE MigrationInfo
+                                       SET
+                                        Name = @Name,
+                                        MigrationCompleted = @MigrationCompleted,
+                                        TotalMigrationTimeInMs = @TotalMigrationTimeInMs,
+                                        CompletedAtUtc = @CompletedAtUtc
+                                       WHERE id=@Id";
+                await db.ExecuteAsync(updateQuery, new
+                {
+                    Id = id,
+                    Name = info.Name,
+                    MigrationCompleted = info.MigrationCompleted,
+                    TotalMigrationTimeInMs = info.TotalMigrationTimeInMs,
+                    CompletedAtUtc = info.CompletedAtUtc
+                });
+            }
+            else
+            {
+                await db.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS MigrationInfo (
+                                    id serial not null
+                                    constraint migrationinfo_pkey
+                                    primary key,
+                                    Name text,
+                                    MigrationCompleted boolean,
+                                    TotalMigrationTimeInMs int8,
+                                    CompletedAtUtc timestamp
+                                    );");
+
+                string insertQuery = @"INSERT INTO MigrationInfo(
+                                        Name,
+                                        MigrationCompleted,
+                                        TotalMigrationTimeInMs,
+                                        CompletedAtUtc) 
+                                   VALUES (
+                                        @Name,
+                                        @MigrationCompleted,
+                                        @TotalMigrationTimeInMs,
+                                        @CompletedAtUtc)";
+
+                await db.ExecuteAsync(insertQuery, new
+                {
+                    Name = info.Name,
+                    MigrationCompleted = info.MigrationCompleted,
+                    TotalMigrationTimeInMs = info.TotalMigrationTimeInMs,
+                    CompletedAtUtc = info.CompletedAtUtc
+                });
+
+
+            }
         }
 
-        public Task<bool> HasMigrationBeenFullyCompletedAsync(IMigrationContext context, string migrationName)
+        public async Task<bool> HasMigrationBeenFullyCompletedAsync(IMigrationContext context, string migrationName)
         {
-            throw new NotImplementedException();
+            var info = await GetMigrationInfoAsync(context, migrationName);
+
+            return (info != default(MigrationInfo) && info.MigrationCompleted);
         }
     }
 }
