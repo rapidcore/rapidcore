aba0365f4c2fb1f8cef6cbbcc3ce0ee3ce7e8ee3
Rui Martins <rui@skarp.dk>
2018-02-28 11:34:11 +0100

    Add ConnectionProviderTest

---
+++ src/postgresql/test-unit/ConnectionProviderTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Data;
+using FakeItEasy;
+using Xunit;
+using RapidCore.PostgreSql;
+
+namespace unittests
+{
+    public class ConnectionProviderTests
+    {
+        private readonly PostgreSqlConnectionProvider provider;
+        private readonly IDbConnection db1;
+        private readonly IDbConnection db2;
+
+        public ConnectionProviderTests()
+        {
+            db1 = A.Fake<IDbConnection>();
+            db2 = A.Fake<IDbConnection>();
+
+            provider = new PostgreSqlConnectionProvider();
+        }
+
+        [Fact]
+        public void Default_Throws_ifEmpty()
+        {
+            var actual = Record.Exception(() => provider.Default());
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+
+        [Fact]
+        public void Default_Throws_ifNoDefaultDefined()
+        {
+            provider.Add("one", db1, false);
+
+            var actual = Record.Exception(() => provider.Default());
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+
+        [Fact]
+        public void Default_Returns_Default()
+        {
+            provider.Add("one", db1, true);
+
+            var actual = provider.Default();
+
+            Assert.Same(db1, actual);
+        }
+
+        [Fact]
+        public void Named_Throws_ifEmpty()
+        {
+            var actual = Record.Exception(() => provider.Named("DonkeyKong"));
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+
+        [Fact]
+        public void Named_Throws_ifNotDefined()
+        {
+            provider.Add("one", db1, false);
+            provider.Add("two", db2, false);
+
+            var actual = Record.Exception(() => provider.Named("NotDefined...NotEvenALittleBit...IMean..Seriously!!"));
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+
+        [Fact]
+        public void Named_Returns()
+        {
+            provider.Add("one", db1, false);
+            provider.Add("two", db2, false);
+
+            var actual = provider.Named("one");
+
+            Assert.Same(db1, actual);
+        }
+
+        [Fact]
+        public void Add_Throws_ifAlreadyDefined()
+        {
+            provider.Add("one", db1, false);
+
+            var actual = Record.Exception(() => provider.Add("one", db2, false));
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+
+        [Fact]
+        public void Add_default_Throws_ifDefaultHasAlreadyBeenDefined()
+        {
+            provider.Add("one", db1, true);
+
+            var actual = Record.Exception(() => provider.Add("two", db2, true));
+
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<Exception>(actual);
+        }
+    }
+
+}
