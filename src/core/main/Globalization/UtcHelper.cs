8d1c5a58fea3be02b97ee597aaae28816ac290b6
Patrick Boisen <patrick@skarp.dk>
2018-03-26 14:43:56 +0200

    Add UtcHelper with only a "Now" method

---
+++ src/core/main/Globalization/UtcHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RapidCore.Globalization
+{
+    /// <summary>
+    /// Knows how to handle UTC.
+    /// </summary>
+    public class UtcHelper
+    {
+        /// <summary>
+        /// Get the current date and time in UTC
+        /// </summary>
+        public virtual DateTime Now()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
