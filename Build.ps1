d2bb67abb5523f607952139ca7590d8532225cd2
Nicklas Laine Overgaard <nicklas@skarp.dk>
2018-05-15 11:07:26 +0200

    Fix ps1 build script variable references

--- Build.ps1
+++ Build.ps1
@@ -61,7 +61,7 @@ if ( (!$Env:APPVEYOR_REPO_TAG) -or ( $Env:APPVEYOR_REPO_TAG -ne "true") ) {
 ##
 # Update all packages to use nuget reference and pack them up as nugets
 ##
-$projectsToBuild = '.\src\core\main\rapidcore.csproj', '.\src\google-cloud\main\rapidcore.google-cloud.csproj', '.\src\mongo\main\rapidcore.mongo.csproj', '.\src\postgresql\main\rapidcore.postgresql.csproj', '.\src\redis\main\rapidcore.redis.csproj', '.\src\xunit\main\rapidcore.xunit.csproj'
+$mainProjects = '.\src\core\main\rapidcore.csproj', '.\src\google-cloud\main\rapidcore.google-cloud.csproj', '.\src\mongo\main\rapidcore.mongo.csproj', '.\src\postgresql\main\rapidcore.postgresql.csproj', '.\src\redis\main\rapidcore.redis.csproj', '.\src\xunit\main\rapidcore.xunit.csproj'
 
 foreach ($project in $mainProjects) {
     Use-NuGetReference $project $version
