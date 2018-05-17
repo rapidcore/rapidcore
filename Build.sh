19137e7483e400ace1b31f9b5f3ce33c3ce53098
Nicklas Laine Overgaard <nicklas@skarp.dk>
2018-05-17 11:04:16 +0200

    Add bash || exit guards so that build script terminates on error

--- Build.sh
+++ Build.sh
@@ -6,31 +6,31 @@ dotnet restore
 # Build the core and test
 dotnet build -c Release -f netstandard1.6 src/core/main/rapidcore.csproj \
 && dotnet test src/core/test-unit/unittests.csproj -c Release -f netcoreapp2.0 \
-&& dotnet test src/core/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0
+&& dotnet test src/core/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0 || { echo 'main project build / test failure' ; exit 1; }
 
 # Build google-cloud and test
 dotnet build -c Release -f netstandard1.6 src/google-cloud/main/rapidcore.google-cloud.csproj \
-&& dotnet test src/google-cloud/test-unit/unittests.csproj -c Release -f netcoreapp1.1 \
-&& dotnet test src/google-cloud/test-functional/functionaltests.csproj -c Release -f netcoreapp1.1
+&& dotnet test src/google-cloud/test-unit/unittests.csproj -c Release -f netcoreapp2.0 \
+&& dotnet test src/google-cloud/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0 || { echo 'google-cloud project build / test failure' ; exit 1; }
 
 # Build mongo and test
 dotnet build -c Release -f netstandard1.6 src/mongo/main/rapidcore.mongo.csproj \
 && dotnet test src/mongo/test-unit/unittests.csproj -c Release -f netcoreapp2.0 \
-&& dotnet test src/mongo/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0
+&& dotnet test src/mongo/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0 || { echo 'mongo project build / test failure' ; exit 1; }
 
 # Build postgreSql and test
 dotnet build -c Release -f netstandard1.6 src/postgresql/main/rapidcore.postgresql.csproj \
 && dotnet test src/postgresql/test-unit/unittests.csproj -c Release -f netcoreapp2.0 \
-&& dotnet test src/postgresql/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0
+&& dotnet test src/postgresql/test-functional/functionaltests.csproj -c Release -f netcoreapp2.0 || { echo 'postgresql project build / test failure' ; exit 1; }
 
 # Build redis and test
 dotnet build -c Release -f netstandard1.6 src/redis/main/rapidcore.redis.csproj \
 && dotnet test src/redis/test-unit/unittests.csproj -c Release -f netcoreapp2.0 \
-&& dotnet test src/redis/test-functional/functionaltest.csproj -c Release -f netcoreapp2.0
+&& dotnet test src/redis/test-functional/functionaltest.csproj -c Release -f netcoreapp2.0 || { echo 'redis project build / test failure' ; exit 1; }
 
 # Build xunit extensions and test
 dotnet build -c Release -f netstandard1.6 src/xunit/main/rapidcore.xunit.csproj \
-&& dotnet test src/xunit/test-unit/unittests.csproj -c Release -f netcoreapp2.0
+&& dotnet test src/xunit/test-unit/unittests.csproj -c Release -f netcoreapp2.0 || { echo 'xunit helpers project build / test failure' ; exit 1; }
 
 # Build documentation
 docker build -t rapidcore:mkdocs -f Dockerfile.mkdocs .
